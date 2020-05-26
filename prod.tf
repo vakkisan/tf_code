6638071761099f9b40e826d58fc6e36acf5989b8
vinay <vnaireddy1@gmail.com>
2020-05-26 08:39:45 +0530

    random

--- prod.tf
+++ prod.tf
@@ -39,6 +39,8 @@ resource "aws_security_group" "prod_web" {
 }
 
 resource "aws_instance" "prod_web" {
+ count = 2
+
  ami 		   = "ami-055067af7816e32b0"
  instance_type = "t2.nano"
 
@@ -52,10 +54,45 @@ resource "aws_instance" "prod_web" {
 }
 
 resource "aws_eip" "prod_web" {
-	instance = aws_instance.prod_web.id
 
 	tags = {
     "Terraform" : "true"
     }
+}
+
+resource "aws_eip_association" "prod_web" {
+	instance_id = aws_instance.prod_web.0.id
+	allocation_id = aws_eip.prod_web.id
+}
+
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "us-west-2a"
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_default_subnet" "default_az2" {
+  availability_zone = "us-west-2b"
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_elb" "prod_web" {
+  name            = "prod-web"
+  instances		  = aws_instance.prod_web.*.id
+  subnets         = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  security_groups = [aws_security_group.prod_web.id]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+  tags = {
+    "Terraform" : "true"
+  }
 }
 
